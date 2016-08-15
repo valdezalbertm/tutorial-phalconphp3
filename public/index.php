51a0fe91ca3ae2ec23f03a320cab481cfde26ec3
Albert Valdez <valdezalbertm@gmail.com>
2016-08-16 02:16:19 +0800

    [Tutorial 1] - Beautiful URLs - Bootstrap - Autoloaders - Dependency Management - Creating a Controller - Sending output to a view - Designing a sign up form - Creating a Model - Setting a Database Connection - Storing data using models

---
+++ public/index.php
@@ -0,0 +1,55 @@
+<?php
+
+use Phalcon\Loader;
+use Phalcon\Mvc\View;
+use Phalcon\Mvc\Application;
+use Phalcon\Di\FactoryDefault;
+use Phalcon\Mvc\Url as UrlProvider;
+use Phalcon\Db\Adapter\Pdo\Mysql as DbAdapter;
+
+try {
+
+    // Register an autoloader
+    $loader = new Loader();
+    $loader->registerDirs(array(
+        '../app/controllers/',
+        '../app/models/'
+    ))->register();
+
+    // Create a DI
+    $di = new FactoryDefault();
+
+    // Setup the database service
+    $di->set('db', function () {
+        return new DbAdapter(array(
+            "host"     => "localhost",
+            "username" => "root",
+            "password" => "",
+            "dbname"   => "phalcon3"
+        ));
+    });
+
+    // Setup the view component
+    $di->set('view', function () {
+        $view = new View();
+        $view->setViewsDir('../app/views/');
+        return $view;
+    });
+
+    // Setup a base URI so that all generated URIs include the "tutorial" folder
+    $di->set('url', function () {
+        $url = new UrlProvider();
+        $url->setBaseUri('/tutorial-phalconphp3/');
+        return $url;
+    });
+
+    $application = new Application($di);
+
+    // Handle the request
+    $response = $application->handle();
+
+    $response->send();
+
+} catch (\Exception $e) {
+     echo "Exception: ", $e->getMessage();
+}
